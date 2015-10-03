c9f17ec550bbc0bee961fc121fd0cce9157e34d6
Andy Hsu <khhsu1333@gmail.com>
2015-10-03 14:28:08 +0800

    Implement recursive version of Fibonacci.

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -4,11 +4,11 @@ extern int fibonacci(int x);
 
 int main(int argc, char **argv)
 {
-  int number=0;
-  int result=0;
+	 int number=0;
+	 int result=0;
 
-  scanf("%d",&number);
-  result = fibonacci(number);   
-  printf("The fibonacci sequence at %d is: %d\n", number, result);
+	 scanf("%d",&number);
+	 result = fibonacci(number);   
+	 printf("The fibonacci sequence at %d is: %d\n", number, result);
 }
 
